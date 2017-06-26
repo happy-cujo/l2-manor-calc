3ea09a921bef662f52346cb878b69fc244f57311
Paweł Auguściński <pawel.auguscinski@datainvest.pl>
2017-06-26 17:30:44 +0200

    Moved builded page to /docs folder

--- build/build.js
+++ build/build.js
@@ -33,8 +33,4 @@ webpack(webpackConfig, function (err, stats) {
   }) + '\n\n')
 
   console.log(chalk.cyan('  Build complete.\n'))
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  ))
 })
